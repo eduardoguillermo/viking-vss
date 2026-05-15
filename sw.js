79eda4a01152e80b689475e8617db72e05007d02
eduardoguillermo <70851744+eduardoguillermo@users.noreply.github.com>
2026-05-15 20:00:19 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='viking-vss-202605152221';
-self.addEventListener('install',e=>{self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
