dac2ceda3bdda591f40bb7bd3591bc525bf37b8f
eduardoguillermo <70851744+eduardoguillermo@users.noreply.github.com>
2026-05-26 22:10:32 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='viking-vss-20260527005221681';
+const CACHE='viking-vss-20260527010851556';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
